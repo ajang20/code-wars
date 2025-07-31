6277d5694662847d717cc80f2520542afebe3e60
ajang20 <ajangkorenilo8@gmaile.com>
2025-07-31 14:06:41 +0200

    8kyu Katas

--- 8kyu.js
+++ 8kyu.js
@@ -10,3 +10,63 @@ class Hero {
         
     }
 }
+
+//8 kyu Surface Area and Volume of a Box
+function getSize(width, height, depth){
+  let volume = width*height*depth;
+  let totalSurfaceArea= (2*width*depth)+(2*width*height)+(2*height*depth);
+  
+  return [totalSurfaceArea,volume];
+}
+console.log(getSize(4, 2, 6));
+
+//8 kyu Switch it Up!
+function switchItUp(number){
+switch(number){
+    case 0:
+    return "Zero";
+    case 1:
+    return "One";
+    case 2:
+    return "Two";
+    case 3:
+    return "Three";
+    case 4:
+    return "Four";
+    case 5:
+    return "Five";
+    case 6:
+    return "Six";
+    case 7:
+    return "Seven";
+    case 8:
+    return 'Eight';
+    case 9:
+    return "Nine";
+}
+}
+console.log(switchItUp(2))
+
+//8 kyu simple calculator
+function calculator(a, b, sign) {
+  // Check if a and b are numbers
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    return 'unknown value';
+  }
+  
+  // Perform the operation based on the sign
+  switch (sign) {
+    case '+':
+      return a + b;
+    case '-':
+      return a - b;
+    case '*':
+      return a * b;
+    case '/':
+      return a / b;
+    case '%':
+      return a % b;
+    default:
+      return 'unknown value';
+  }
+}
