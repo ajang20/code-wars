0cd03789649f679d079b2a97c85a2900ac8fc52c
ajang20 <ajangkorenilo8@gmail.com>
2025-08-05 16:55:17 +0200

    update

--- 6kyu.js
+++ 6kyu.js
@@ -16,3 +16,19 @@ function toCamelCase(str){
   return newStr;
  }
  console.log(toCamelCase("the-stealth-warrior"));
+
+ /*
+ 6 kyu Split Strings
+ Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+
+Examples:
+
+* 'abc' =>  ['ab', 'c_']
+* 'abcdef' => ['ab', 'cd', 'ef']
+
+ */
+function solution(str){
+  let newStr=str.length%2===0?str:str+"_";
+const pairs= newStr.match(/.{2}/g) || []
+   return pairs;
+}
