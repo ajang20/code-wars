32267ee575e1ca3602243f0b4e11422517931a16
Ajang Samuel <ajangkorenilo8@gmail.com>
2025-08-24 20:44:57 +0200

    8kyu updates

--- 8kyu.js
+++ 8kyu.js
@@ -206,6 +206,5 @@ return sentence.toLowerCase().includes("english");
 // 8 kyu Simple multiplication
 // This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
 function simpleMultiplication(number) {
-  //
    return number%2===0?number*8:number*9;
 }
