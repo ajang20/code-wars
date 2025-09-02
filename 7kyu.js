7a4ce1711993ca408132e38569c88364c17205fd
Ajang Samuel <ajangkorenilo8@gmail.com>
2025-09-02 15:50:50 +0200

    updates

--- 7kyu.js
+++ 7kyu.js
@@ -99,3 +99,30 @@ C = union(A,B) // -> {1,2,3}
 function union(s1, s2){
     return new Set([...s1,...s2]);
 }
+
+// 7 kyu reverseIt
+// You have to create a function named reverseIt.
+
+// Write your function so that in the case a string or a number is passed in as the data , you will return the data in reverse order. If the data is any other type, return it as it is.
+
+// Examples of inputs and subsequent outputs:
+
+// "Hello" -> "olleH"
+
+// "314159" -> "951413"
+
+// [1,2,3] -> [1,2,3]
+function reverseIt(data) {
+  if (typeof data === 'string') {
+    return [...data].reverse().join("");
+  }
+  else if (Array.isArray(data)) {
+    return data.slice().reverse();
+  } 
+  else if (typeof data==="number"){
+   return Number((data+"").split("").reverse().join("")) 
+  }
+else{
+  return data;
+}
+}
