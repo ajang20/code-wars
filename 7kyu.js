3f72bc79dc700704b46c99cb90663eac76d06653
Ajang Samuel <ajangkorenilo8@gmail.com>
2025-09-04 13:24:43 +0200

    updates

--- 7kyu.js
+++ 7kyu.js
@@ -126,3 +126,14 @@ else{
   return data;
 }
 }
+
+
+// 7 kyu Flatten
+// Write a function that flattens an Array of Array objects into a flat Array. Your function must only do one level of flattening.
+
+// flatten([1,2,3]) // => [1,2,3]
+// flatten([[1,2,3],["a","b","c"],[1,2,3]])  // => [1,2,3,"a","b","c",1,2,3]
+// flatten([[[1,2,3]]]) // => [[1,2,3]]
+var flatten = function (array){
+  return array.flat();
+}
