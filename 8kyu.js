0dd463023e531b951cbea99efa4b0b025dbb45d0
Ajang Samuel <ajangkorenilo8@gmail.com>
2025-09-09 13:34:59 +0200

    updates

--- 8kyu.js
+++ 8kyu.js
@@ -234,3 +234,12 @@ return name.length>=3?
 [...name.split(" "),...result]
 :name.split(" ");
 }
+// 8 kyu Convert number to reversed array of digits
+// Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+// Example (Input => Output):
+// 35231 => [1,3,2,5,3]
+// 0     => [0]
+function digitize(n) {
+  return (n+"").split("").reverse().map(ele=>ele*1);
+}
