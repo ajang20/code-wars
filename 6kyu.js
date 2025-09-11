cb08a113e752157d1a296ec7cff20de5fbeb4fa8
Ajang Samuel <ajangkorenilo8@gmail.com>
2025-09-11 13:56:24 +0200

    6kyu updates

--- 6kyu.js
+++ 6kyu.js
@@ -32,3 +32,22 @@ function solution(str){
 const pairs= newStr.match(/.{2}/g) || []
    return pairs;
 }
+
+// 6 kyu Consecutive Count
+// I want to know the size of the longest consecutive elements of X in Y. You will receive two arguments: items and key. Return the length of the longest segment of consecutive keys in the given items.
+
+// Notes:
+
+// The items and the key will be either an integer or a string (consisting of letters only)
+// If the key does not appear in the items, return 0
+// Examples
+// 90000, 0           -->  4
+// "abcdaaadse", "a"  -->  3
+// "abcdaaadse", "z"  -->  0
+function getConsectiveItems(items, key){
+    let regexp= new RegExp(`${key+""}+`,"g");
+    let newStr=items+"";
+    let matches= newStr.match(regexp);
+  if (!matches) return 0;
+    return matches.length<=1?matches[0].length:Math.max(...(matches.map(ele=>ele.length)));
+}
