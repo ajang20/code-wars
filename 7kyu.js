cd9c2c0c95b3293250c8da265f7356fd1af5da02
Ajang Samuel <ajangkorenilo8@gmail.com>
2025-09-12 14:27:52 +0200

    7kyu updates

--- 7kyu.js
+++ 7kyu.js
@@ -188,3 +188,23 @@ function automorphic(n){
 function power(base, exponent){
 return base**exponent;
 }
+
+// 7 kyu Credit Card Mask
+// Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+
+// Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+// Examples (input --> output):
+// "4556364607935616" --> "############5616"
+//      "64607935616" -->      "#######5616"
+//                "1" -->                "1"
+//                 "" -->                 ""
+
+// // "What was the name of your first pet?"
+// "Skippy" --> "##ippy"
+// "Nananananananananananananananana Batman!" --> "################################man!"
+function maskify(cc) {
+  if(cc.length<=3) return cc;
+let lastStr=cc.slice(cc.length-4);
+  return lastStr.padStart(cc.length,"#");
+}
