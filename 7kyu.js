2900bf262ced3116085cbca1994c19c31eee5281
Ajang Samuel <ajangkorenilo8@gmail.com>
2025-09-17 13:12:38 +0200

    7ku Updates

--- 7kyu.js
+++ 7kyu.js
@@ -208,3 +208,14 @@ function maskify(cc) {
 let lastStr=cc.slice(cc.length-4);
   return lastStr.padStart(cc.length,"#");
 }
+
+// 7 kyu shorter concat [reverse longer]
+// Given 2 strings, a and b, return a string of the form: shorter+reverse(longer)+shorter.
+
+// In other words, the shortest string has to be put as prefix and as suffix of the reverse of the longest.
+
+// Strings a and b may be empty, but not null (In C# strings may also be null. Treat them as if they are empty.).
+// If a and b have the same length treat a as the longer producing b+reverse(a)+b
+function shorterReverseLonger(a,b){
+  return a.length>=b.length?`${b}${a.split("").reverse().join("")}${b}`:`${a}${b.split("").reverse().join("")}${a}`;
+}
