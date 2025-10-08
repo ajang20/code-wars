bba94bf5e36a07dcb974e0fc72a1d4e665adc1f6
Ajang Samuel <ajangkorenilo8@gmail.com>
2025-10-08 14:47:54 +0200

    New Kata's Updates

--- 8kyu.js
+++ 8kyu.js
@@ -426,14 +426,15 @@ function hello(name) {
   return !name?"Hello, World!" :`Hello, ${name[0].toUpperCase()}${name.slice(1).toLowerCase()}!`;
 }
 
-// 7 kyu Leap Years
-// In this kata you should simply determine, whether a given year is a leap year or not. In case you don't know the rules, here they are:
+// 8 kyu Quarter of the year
+// Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
 
-// Years divisible by 4 are leap years,
-// but years divisible by 100 are not leap years,
-// but years divisible by 400 are leap years.
-// Tested years are in range 1600 ≤ year ≤ 4000.
+// For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
 
-function isLeapYear(year) {
-  return year%4===0 && year%100!==0 || year%400===0? true:false;
+// Constraint:
+
+// 1 <= month <= 12
+
+const quarterOf = (month) => {
+ return month<=3?1:month<=6?2:month<=9?3:month<=12?4:false;
 }
