baf51084b0e99ebf5f9e1f9fe00cda88656de6b3
Ajang Samuel <ajangkorenilo8@gmail.com>
2025-10-08 14:48:58 +0200

    New Kata's Updates

--- 8kyu.js
+++ 8kyu.js
@@ -438,3 +438,26 @@ function hello(name) {
 const quarterOf = (month) => {
  return month<=3?1:month<=6?2:month<=9?3:month<=12?4:false;
 }
+
+// 8 kyu Check same case
+// Write a function that will check if two given characters are the same case.
+
+// If either of the characters is not a letter, return -1
+// If both characters are the same case, return 1
+// If both characters are letters, but not the same case, return 0
+// Examples
+// 'a' and 'g' returns 1
+
+// 'A' and 'C' returns 1
+
+// 'b' and 'G' returns 0
+
+// 'B' and 'g' returns 0
+
+// '0' and '?' returns -1
+function sameCase(a, b){
+  return typeof a!=="string" || typeof b!=="string" 
+  || !/[a-zA-Z]/.test(a) || !/[a-zA-Z]/.test(b)?-1:
+  a===a.toLowerCase() && b===b.toLowerCase() ||
+   a===a.toUpperCase() && b===b.toUpperCase()  ?1:0;
+}
