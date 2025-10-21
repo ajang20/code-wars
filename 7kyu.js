bd5c655553eeeb76bbcda1b931ad791e6620877a
Ajang Samuel <ajangkorenilo8@gmail.com>
2025-10-21 14:55:23 +0200

    7kyu New Katas

--- 7kyu.js
+++ 7kyu.js
@@ -260,3 +260,22 @@ function removeBMW(str){
       }
   return str.replace(/[BMW]/ig,"").trim();
   }
+
+//   7 kyu Sum of array singles
+//   In this Kata, you will be given an array of numbers in which two numbers occur once and the rest occur only twice. Your task will be to return the sum of the numbers that occur only once.
+
+// For example, repeats([4,5,7,5,4,8]) = 15 because only the numbers 7 and 8 occur once, and their sum is 15. Every other number occurs twice.
+
+// More examples in the test cases.
+
+// Good luck!
+
+// If you like this Kata, please try:
+
+// Sum of prime-indexed elements
+
+// Sum of integer combinations
+
+function repeats(arr){
+return arr.filter((ele,i,arr)=>arr.indexOf(ele)===arr.lastIndexOf(ele)).reduce((curr,next)=>curr+next,0);
+}
