61de1f70a089ca45f866b976f49f8d3922d1de52
Ajang Samuel <ajangkorenilo8@gmail.com>
2025-10-31 14:13:28 +0200

    6kyu.js

--- 6kyu.js
+++ 6kyu.js
@@ -108,3 +108,19 @@ return a.filter(ele=>!b.includes(ele));
 
 // Too hard? This one's a bit easier :)
 sd=x=>[...x+""].map(d=>d*d).join``*1
+
+
+// 6 kyu Sorted Arrays
+// Given any number of arrays each sorted in ascending order, find the nth smallest number of all their elements.
+
+// All the arguments except the last will be arrays, the last argument is n.
+
+// nthSmallest([1,5], [2], [4,8,9], 4) // returns 5 because it's the 4th smallest value
+// Be mindful of performance.
+
+function nthSmallest(...arr) {
+  let n= arr[arr.length-1];
+  let array= arr.slice(0,arr.length-1);
+  return array.flat(Infinity).sort((a,b)=>a-b)[n-1];
+  
+}
