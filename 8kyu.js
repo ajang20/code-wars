55eecd65181713a25cf7eb8c8f4b7acc866e9eb1
Ajang Samuel <ajangkorenilo8@gmail.com>
2025-11-03 14:56:16 +0200

    7kyu Updates!

--- 8kyu.js
+++ 8kyu.js
@@ -512,3 +512,17 @@ function multiTable(number) {
   }
   return result;
 }
+
+// 8 kyu The 'if' function
+// Create a function that takes three arguments:
+
+// a value to be evaluated for truthiness.
+// a function to execute if the first argument is truthy.
+// a function to execute if the first argument is falsy.
+// If the first argument evaluates to truthy, call the second argument (a function). If it evaluates to falsy, call the third argument instead (also a function).
+
+// Note: The first argument can be any value, since most languages allow any value to be interpreted as truthy or falsy. In strongly typed languages, however, it will typically be a boolean.
+
+function _if(bool, func1, func2) {
+ return bool?func1():func2();
+}
