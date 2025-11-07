1f86f2c16a3633c0ec656bf46fc9f6a0bcfd059f
Ajang Samuel <ajangkorenilo8@gmail.com>
2025-11-07 20:21:05 +0200

    8kyu.js

--- 8kyu.js
+++ 8kyu.js
@@ -571,3 +571,27 @@ function validateHello(greetings) {
  let res =  /hello|ciao|salut|hallo|hola|ahoj|czesc/ig.test(greetings); 
   return res;
 }
+
+// 8 kyu Sum without highest and lowest number
+// Task
+// Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+
+// The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+
+// Mind the input validation.
+
+// Example
+// { 6, 2, 1, 8, 10 } => 16
+// { 1, 1, 11, 2, 3 } => 6
+// Input validation
+// If an empty value ( null, None, Nothing, nil etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
+
+function sumArray(array) {
+  if(!array || array.length<=2) return 0;
+  
+  const sort= array.slice().sort((a,b)=>a-b);
+  sort.pop();
+  sort.shift();
+  
+  return sort.reduce((sum,num)=>sum+num,0);
+}
