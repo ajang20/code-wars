1846392f953ae2de489290101a221893d25e4404
Ajang Samuel <ajangkorenilo8@gmail.com>
2025-11-08 13:24:21 +0200

    8kyu Updates

--- 8kyu.js
+++ 8kyu.js
@@ -603,3 +603,29 @@ function sumArray(array) {
 function swapValues(arr) {
   return [arr[0],arr[1]]=[arr[1],arr[0]];
 }
+
+// 8 kyu Flick Switch
+// Task
+// Create a function that always returns True/true for every item in a given list.
+// However, if an element is the word 'flick', switch to always returning the opposite boolean value.
+
+// Examples
+// ['codewars', 'flick', 'code', 'wars'] ➞ [True, False, False, False]
+
+// ['flick', 'chocolate', 'adventure', 'sunshine'] ➞ [False, False, False, False]
+
+// ['bicycle', 'jarmony', 'flick', 'sheep', 'flick'] ➞ [True, True, False, False, True]
+// Notes
+// "flick" will always be given in lowercase.
+// A list may contain multiple flicks.
+// Switch the boolean value on the same element as the flick itself.
+
+function flickSwitch(arr){
+ let bool = true;
+   
+  return arr.map(word=>{
+    if(word==="flick") bool=!bool;
+    return bool;
+  });
+  
+}
