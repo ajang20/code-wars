0b43b0b58f0b217d6281ffb168c797eedf1e3292
Ajang Samuel <ajangkorenilo8@gmail.com>
2025-11-10 15:10:30 +0200

    5kyu Updates

--- 5kyu.js
+++ 5kyu.js
@@ -35,3 +35,14 @@ function pigIt(str){
   let result= str.split(" ").filter(ele=>ele!==char).map(ele=>ele.slice(1)+ele.at(0)+"ay").join(" ");
   return result.includes(`${char}`)?str.split(" ").map(ele=>ele.slice(1)+ele.at(0)+"ay").join(" "):`${result} ${char}`;
 }
+// 5 kyu flatten()
+// For this exercise you will create a global flatten method. The method takes in any number of arguments and flattens them into a single array. If any of the arguments passed in are an array then the individual objects within the array will be flattened so that they exist at the same level as the other arguments. Any nested arrays, no matter how deep, should be flattened into the single array result.
+
+// The following are examples of how this function would be used and what the expected results would be:
+
+// flatten(1, [2, 3], 4, 5, [6, [7]]) ==> [1, 2, 3, 4, 5, 6, 7]
+// flatten('a', ['b', 2], 3, 666, [[4], ['c']]) ==> ['a', 'b', 2, 3, 666, 4, 'c']
+
+function flatten(...args) {
+    return args.flat(Infinity);
+}
