b93ef0f605097f2dbebdd8b66033e38be5b535da
Ajang Samuel <ajangkorenilo8@gmail.com>
2025-11-10 15:14:19 +0200

    7kyu Updates

--- 7kyu.js
+++ 7kyu.js
@@ -414,3 +414,17 @@ function isKiss( words ){
   let max= Math.max(...(words.split(" ").map(ele=>ele.length)));
  return max>l?"Keep It Simple Stupid":"Good work Joe!";
 }
+
+// 7 kyu Descending Order
+// Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+// Examples:
+// Input: 42145 Output: 54421
+
+// Input: 145263 Output: 654321
+
+// Input: 123456789 Output: 987654321
+
+function descendingOrder(n){
+  return (n+"").split("").map(ele=>ele*1).sort((a,b)=>b-a).map(ele=>ele+"").join("")*1;
+}
