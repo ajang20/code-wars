aacd9f9219b47bc9c7b15fe430d7242e58a12d82
Ajang Samuel <ajangkorenilo8@gmail.com>
2025-11-18 15:29:04 +0200

    7kyu.js Updates

--- 7kyu.js
+++ 7kyu.js
@@ -525,7 +525,6 @@ prod *= i;
 // I have also created other katas. Take a look if you enjoyed this kata!
 
 
-
 function dontGiveMeFive(start, end){
   let array =[];
   
@@ -535,4 +534,17 @@ function dontGiveMeFive(start, end){
   
   let arr=  array.filter(ele=>!ele.includes("5"));
   return arr.length ;
+}
+
+// 7 kyu Odds-Index
+// You are given an array with several "even" words, one "odd" word, and some numbers mixed in.
+
+// Determine if any of the numbers in the array is the index of the "odd" word. If so, return true, otherwise false.
+
+// Algorithms
+
+function oddBall(arr){
+  let index = arr.indexOf("odd");
+  let bool = arr.includes(index);
+    return bool;
 }
