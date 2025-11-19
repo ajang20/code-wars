75838f444d82a605a23ea1e587f787209f84ec6d
Ajang Samuel <ajangkorenilo8@gmail.com>
2025-11-19 11:14:41 +0200

    7kyu Updates

--- 7kyu.js
+++ 7kyu.js
@@ -548,3 +548,29 @@ function oddBall(arr){
   let bool = arr.includes(index);
     return bool;
 }
+
+// 7 kyu Build a square
+// I will give you an integer. Give me back a shape that is as long and wide as the integer. The integer will be a whole number between 1 and 50.
+
+// Example
+// n = 3, so I expect a 3x3 square back just like below as a string:
+
+// +++
+// +++
+// +++
+
+function generateShape(integer){
+  let sum="";
+  let int = "";
+  
+  for(let i=0;i<integer;i++){
+   for(let i=0;i<integer;i++){
+        int +="+"
+       if(int.length===integer){
+           sum+=`${int}\n`;
+           int = ""
+       }
+   } 
+  }
+return sum.slice(0,sum.length-1);
+}
