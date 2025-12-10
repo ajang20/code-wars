38d13d7245443a26828573edc9be879e741966de
Ajang Samuel <ajangkorenilo8@gmail.com>
2025-12-10 14:57:23 +0200

    Code wars updates

---
+++ JavaScript Katas/7kyu/A Promise is a Promise.js
@@ -0,0 +1,6 @@
+// 7 kyu A Promise is a Promise
+
+// Create a resolved javascript Promise that will return 'Hello World!'.
+function promiseHelloWorld() {
+ return Promise.resolve("Hello World!")
+ }
