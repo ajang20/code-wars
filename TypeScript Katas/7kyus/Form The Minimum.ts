c0e51b885273808683a1ae83a4ab357ebb0756d5
Ajang Samuel <ajangkorenilo8@gmail.com>
2025-12-15 23:27:48 +0200

    TS Code wars updates

---
+++ TypeScript Katas/7kyus/Form The Minimum.ts
@@ -0,0 +1,22 @@
+// 7 kyu Form The Minimum
+
+// Task
+// Given a list of digits, return the smallest number that could be formed from these digits, using the digits only once (ignore duplicates). Only positive integers in the range of 1 to 9 will be passed to the function.
+
+// Examples
+// [1, 3, 1] ==> 13
+// [5, 7, 5, 9, 7] ==> 579
+// [1, 9, 3, 1, 7, 4, 6, 6, 7]  ==> 134679
+// Playing with Numbers Series
+
+// Playing With Lists/Arrays Series
+
+// Bizarre Sorting-katas
+
+// For More Enjoyable Katas
+
+export const minValue = (values: number[]): number => {
+  let unique = new Set(values);
+  
+  return Number([...unique].sort((a,b)=>a-b).join(""))
+};
