aeec0ef5a8f2f4f092f8ddbf977d749cab6ac9f3
Ajang Samuel <ajangkorenilo8@gmail.com>
2025-12-15 23:20:23 +0200

    Code wars updates

---
+++ JavaScript Katas/7kyu/Form the Minimum.js
@@ -0,0 +1,23 @@
+// 7 kyu Form The Minimum
+
+// Task
+// Given a list of digits, return the smallest number that could be formed from these digits, using the digits only once (ignore duplicates). Only positive integers in the range of 1 to 9 will be passed to the function.
+
+// Examples
+// [1, 3, 1] ==> 13
+// [5, 7, 5, 9, 7] ==> 579
+// [1, 9, 3, 1, 7, 4, 6, 6, 7]  ==> 134679
+// Playing with Numbers Series
+
+// Playing With Lists/Arrays Series
+
+// Bizarre Sorting-katas
+
+// For More Enjoyable Katas
+
+function minValue(values){
+  let noduplicates = new Set(values);
+   
+  let num = Number([...noduplicates].map(ele=>ele.toString()).sort((a,b)=>a-b).join(""));
+  return num;
+}
