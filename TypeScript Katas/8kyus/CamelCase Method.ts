d033e845da57f3de0bfbf152ecc261c33debb0cc
Ajang Samuel <ajangkorenilo8@gmail.com>
2025-12-18 20:24:51 +0200

    TypeScript Codewars Updates

---
+++ TypeScript Katas/8kyus/CamelCase Method.ts
@@ -0,0 +1,22 @@
+// 6 kyu CamelCase Method
+
+// Write a method (or function, depending on the language) that converts a string to camelCase, that is, all words must have their first letter capitalized and spaces must be removed.
+
+// Examples (input --> output):
+// "hello case" --> "HelloCase"
+// "camel case word" --> "CamelCaseWord"
+// Don't forget to rate this kata! Thanks :)
+
+export function camelCase(str: string): string {
+  return str
+  .split(" ")
+  .filter(Boolean)
+  .map(ele=>{
+    let newStr= ele.toLowerCase();
+    
+  return  newStr[0].toUpperCase() + newStr.slice(1)
+    
+  })
+  .join("")
+  
+}
