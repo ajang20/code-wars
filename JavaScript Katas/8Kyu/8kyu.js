f1d64af74b525077be3e28e1a502b85e3368f884
Ajang Samuel <ajangkorenilo8@gmail.com>
2026-01-10 21:53:13 +0200

    8kyu Javascript Updates

--- JavaScript Katas/8Kyu/8kyu.js
+++ JavaScript Katas/8Kyu/8kyu.js
@@ -519,39 +519,8 @@ function sumArray(array) {
   return sort.reduce((sum,num)=>sum+num,0);
 }
 
-// 8 kyu Swap Values
-// I would like to be able to pass an array with two elements to my swapValues function to swap the values. However it appears that the values aren't changing.
 
-// Can you figure out what's wrong here?
-function swapValues(arr) {
-  return [arr[0],arr[1]]=[arr[1],arr[0]];
-}
 
-// 8 kyu Flick Switch
-// Task
-// Create a function that always returns True/true for every item in a given list.
-// However, if an element is the word 'flick', switch to always returning the opposite boolean value.
-
-// Examples
-// ['codewars', 'flick', 'code', 'wars'] ➞ [True, False, False, False]
-
-// ['flick', 'chocolate', 'adventure', 'sunshine'] ➞ [False, False, False, False]
-
-// ['bicycle', 'jarmony', 'flick', 'sheep', 'flick'] ➞ [True, True, False, False, True]
-// Notes
-// "flick" will always be given in lowercase.
-// A list may contain multiple flicks.
-// Switch the boolean value on the same element as the flick itself.
-
-function flickSwitch(arr){
- let bool = true;
-   
-  return arr.map(word=>{
-    if(word==="flick") bool=!bool;
-    return bool;
-  });
-  
-}
 
 
 
