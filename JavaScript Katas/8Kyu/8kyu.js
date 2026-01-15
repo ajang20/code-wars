6dac2a1a726fe15f8c054f4f20314612a319703f
Ajang Samuel <ajangkorenilo8@gmail.com>
2026-01-15 21:48:50 +0200

    JV code wars updates

--- JavaScript Katas/8Kyu/8kyu.js
+++ JavaScript Katas/8Kyu/8kyu.js
@@ -397,44 +397,9 @@ function convertToCelsius (temperature) {
   return celsius;
 }
 
-// 8 kyu Array plus array
-// I'm new to coding and now I want to get the sum of two arrays... Actually the sum of all their elements. I'll appreciate for your help.
 
-// P.S. Each array includes only integer numbers. Output is a number too.
 
-function arrayPlusArray(arr1, arr2) {
-  return [...arr1,...arr2].reduce((curr,next)=>curr+next,0); //something went wrong
-}
 
-// 8 kyu Multiplication table for number
-// Your goal is to return multiplication table for number that is always an integer from 1 to 10.
-
-// For example, a multiplication table (string) for number == 5 looks like below:
-
-// 1 * 5 = 5
-// 2 * 5 = 10
-// 3 * 5 = 15
-// 4 * 5 = 20
-// 5 * 5 = 25
-// 6 * 5 = 30
-// 7 * 5 = 35
-// 8 * 5 = 40
-// 9 * 5 = 45
-// 10 * 5 = 50
-// P. S. You can use \n in string to jump to the next line.
-
-// Note: newlines should be added between rows, but there should be no trailing newline at the end. If you're unsure about the format, look at the sample tests.
-
-function multiTable(number) {
-  let result = '';
-  for (let i = 1; i <= 10; i++) {
-    let product = i * number;
-    result+= `${i} * ${number} = ${product}`;
-     if (i !== 10) result += '\n';
-//     result += (i + ' * ' + number + ' = ' + product + '\n');
-  }
-  return result;
-}
 
 
 
