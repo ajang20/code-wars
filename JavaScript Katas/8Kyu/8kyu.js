b7858a3189dd31828d7cc6ed2b3d041136c92acd
Ajang Samuel <ajangkorenilo8@gmail.com>
2026-01-18 01:28:54 +0200

    Code wars updates

--- JavaScript Katas/8Kyu/8kyu.js
+++ JavaScript Katas/8Kyu/8kyu.js
@@ -360,42 +360,9 @@ function hello(name) {
 function isLeapYear(year) {
   return year%4===0 && year%100!==0 || year%400===0? true:false;
 }
-// 8 kyu Grasshopper - Basic Function Fixer
-// Fix the function
-// I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number.
 
-// Can you help me fix the function?
 
-function addFive(num) {
-  var total = num + 5;
-  return total;
-}
 
-// 8 kyu Grasshopper - Debug
-// Debug celsius converter
-// Your friend is traveling abroad to the United States so he wrote a program to convert fahrenheit to celsius. Unfortunately his code has some bugs.
-
-// Find the errors in the code to get the celsius converter working properly.
-
-// To convert fahrenheit to celsius:
-
-// celsius = (fahrenheit - 32) * (5/9)
-// Remember that typically temperatures in the current weather conditions are given in whole numbers. It is possible for temperature sensors to report temperatures with a higher accuracy such as to the nearest tenth. Instrument error though makes this sort of accuracy unreliable for many types of temperature measuring sensors.
-
-function weatherInfo (temp) {
-  let c = convertToCelsius(temp);
-  if (c <= 0){
-    return (c + " is freezing temperature");
-    }
-  else{
-    return (c + " is above freezing temperature");
-    }
-}
-
-function convertToCelsius (temperature) {
-  let celsius = (temperature - 32 ) * (5/9);
-  return celsius;
-}
 
 
 
