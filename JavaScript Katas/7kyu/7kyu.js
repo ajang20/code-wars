46d1c8723e467dd738393343815895be0fce9035
ajang20 <ajangkorenilo8@gmail.com>
2026-03-19 16:53:10 +0200

    7kyu Code wars Katas solutions

--- JavaScript Katas/7kyu/7kyu.js
+++ JavaScript Katas/7kyu/7kyu.js
@@ -128,16 +128,6 @@ else{
 }
 
 
-// 7 kyu Flatten
-// Write a function that flattens an Array of Array objects into a flat Array. Your function must only do one level of flattening.
-
-// flatten([1,2,3]) // => [1,2,3]
-// flatten([[1,2,3],["a","b","c"],[1,2,3]])  // => [1,2,3,"a","b","c",1,2,3]
-// flatten([[[1,2,3]]]) // => [[1,2,3]]
-var flatten = function (array){
-  return array.flat();
-}
-
 
 
 
