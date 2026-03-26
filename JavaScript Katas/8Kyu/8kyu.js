151e377d5a8197834d39a0b4b6ea92ffcc7c0000
ajang20 <ajangkorenilo8@gmail.com>
2026-03-26 14:47:07 +0200

    8kyu Code wars Katas solutions

--- JavaScript Katas/8Kyu/8kyu.js
+++ JavaScript Katas/8Kyu/8kyu.js
@@ -284,11 +284,7 @@ function addExtra( listOfNumbers ){
 function shortenToDate(longDate) {
 return longDate.split(" ").slice(0,3).join(" ").replace(/,/,"");
 }
-// 8 kyu Hex to Decimal
-// Complete the function which converts hex number (given as a string) to a decimal number.
-function hexToDec(hexString){
-return parseInt(hexString,16);
-  }
+
 
 
 
