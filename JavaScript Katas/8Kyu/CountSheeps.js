f3700b37c3b75b334fee15db1cec7ffe6d258a69
ajang20 <ajangkorenilo8@gmail.com>
2026-04-06 13:53:14 +0200

    8kyu Code wars Solutions

--- JavaScript Katas/8Kyu/CountSheeps.js
+++ JavaScript Katas/8Kyu/CountSheeps.js
@@ -1,17 +1,4 @@
 /*
-Given an array of integers your solution should find the smallest integer.
-
-For example:
-
-Given [34, 15, 88, 2] your solution will return 2
-Given [34, -345, -1, 100] your solution will return -345
-You can assume, for the purpose of this kata, that the supplied array will not be empty.
-*/
-function findSmallestInt(arr) {
-  return Math.min(...arr);
-}
-
-/*
 Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
 
 For example,
